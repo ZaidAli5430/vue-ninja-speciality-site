052dc6373f8c15dbb29473968c35141ce54b8bab
zaid-ali <zaid.ali5430@gmail.com>
2020-03-18 15:35:53 +0500

    added contents between header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
    <app-header></app-header>
-   
+   <app-ninjas></app-ninjas>
    <app-footer></app-footer>
 </div>
  
@@ -10,10 +10,12 @@
 <script>
 import Header from './components/header.vue'
 import Footer from './components/footer.vue'
+import Content from './components/ninjas.vue'
 export default {
   components:{
     'app-header':Header,
-    'app-footer':Footer
+    'app-footer':Footer,
+    'app-ninjas':Content
   },
   data () {
     return {
