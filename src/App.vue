627e7a214bf21f24620e5c1951865b12f9daecbb
zaid-ali <zaid.ali5430@gmail.com>
2020-03-18 14:59:21 +0500

    Header and footer added

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+   <app-header></app-header>
+   
+   <app-footer></app-footer>
+</div>
+ 
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+export default {
+  components:{
+    'app-header':Header,
+    'app-footer':Footer
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+    
+
+</style>
+
+
