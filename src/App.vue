58d3e2ad33b05b84c1cecbfa64d9a2a5e1a9f9ab
zaid-ali <zaid.ali5430@gmail.com>
2020-03-20 00:32:21 +0500

    added slots

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,34 @@
 <template>
-<div>
-   <app-header v-bind:title="title" @changeTitle="updateTitle($event)"></app-header>
-   <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-   <app-footer v-bind:title="title"></app-footer>
-</div>
- 
+  <div>
+    <form-helper >
+      <div slot="form-header">
+        <h3>Admission Form</h3>
+        <p>Fill the form to apply</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required/>
+        <input type="password" placeholder="password" required>
+      </div>
+      <div slot="form-controls">
+        <button @click = "handleSubmit">Submit</button>
+      </div>
+    </form-helper>
+  </div>
 </template>
 
 <script>
-import Header from './components/header.vue'
-import Footer from './components/footer.vue'
-import Content from './components/ninjas.vue'
+import formHelper from './components/form-helper.vue'
 export default {
   components:{
-    'app-header':Header,
-    'app-footer':Footer,
-    'app-ninjas':Content
+    'form-helper':formHelper
   },
   data () {
     return {
-      ninjas: [
-                {name: 'Ryu', speciality: 'Vue Components', show: false},
-                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-                {name: 'Hitoshi', speciality: 'Click Events', show: false},
-                {name: 'Tango', speciality: 'Conditionals', show: false},
-                {name: 'Kami', speciality: 'Webpack', show: false},
-                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-            ],
-      title:"Vue Ninja"
+      title:'I am a dynamic slot title'
     }
   },
   methods:{
-    updateTitle:function(updatedTitle){
-      this.title = updatedTitle;
-    }
+   
   }
 }
 </script>
